0989d05b50e7e4489d4bf9154eb9cefe091e3b76
hussainfiazmlk <hussainfiazmlk@gmail.com>
2022-10-02 10:47:11 +0500

    create crud operation

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
-import Sequelize from "sequelize";
+import Sequelize, { Op } from "sequelize";
+
+// import _users from "./data/users.json" assert { type: "json" };
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
@@ -15,21 +19,116 @@ const dialect = process.env.DIALECT;
 const connection = new Sequelize(db, user, password, {
   host,
   dialect,
+  define: {
+    freezeTableName: true,
+  },
 });
 
-const User = connection.define("User", {
-  uuid: {
-    type: Sequelize.UUID,
-    primaryKey: true,
-    defaultValue: Sequelize.UUIDV4,
+const User = connection.define(
+  "Users",
+  {
+    uuid: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    firstname: Sequelize.STRING,
+    lastname: Sequelize.STRING,
+    fullname: Sequelize.STRING,
+    email: {
+      type: Sequelize.STRING,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: Sequelize.STRING,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
   },
-  name: Sequelize.STRING,
-  bio: Sequelize.TEXT,
+  {
+    hooks: {
+      beforeCreate: (users) => {
+        users.fullname = `${users.firstname} ${users.lastname}`;
+        console.log("Before create===================================");
+      },
+    },
+  }
+);
+
+// app.get("/", async (req, res) => {
+//   try {
+//     await User.bulkCreate(_users);
+//     res.json("successfully added users");
+//   } catch (error) {
+//     console.log(error);
+//     res.status(404).send(error);
+//   }
+// });
+
+// Create new users
+app.post("/users", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body.user);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+// get all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll({
+      where: {
+        firstname: {
+          [Op.like]: "D%",
+        },
+      },
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+// get single user
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+// update user
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.update(req.body, {
+      where: { uuid: req.params.id },
+    });
+
+    res.status(200).json({ msg: "user updated successfully" });
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+// delete user
+app.delete("/users/:id", async (req, res) => {
+  try {
+    await User.destroy({ where: { uuid: req.params.id } });
+    res.status(200).send({ msg: "user successfully deleted!!" });
+  } catch (error) {
+    res.status(404).send(error);
+  }
 });
 
 try {
-  await connection.sync({ logging: console.log, force: true });
-  await User.create({ name: "Bob", bio: "New bio entry 2" });
+  await connection.sync(); // { logging: console.log, force: true }
   console.log("Successfully connected to the database");
 } catch (err) {
   console.error("Unable to connect to the database:", err);
